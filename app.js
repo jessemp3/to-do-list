0ba03a231887772f1523cc7908eada6bd8eebc96
jesse <nitchsx@gmail.com>
2024-03-29 20:14:05 -0300

    inclusão sla

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/' , (req , res) => {
+  res.send('<h1>Minha lista de tarefas<h1/>')
+})
+
+app.listen(9000, () => {
+  console.log('Servidor iniciado')
+})
