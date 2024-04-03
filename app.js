4eb9d41695958f2875ef5ee8d7d74472c95eb266
jesse <nitchsx@gmail.com>
2024-04-03 00:06:32 -0300

    adição do framework bulma ao proejeto , adição da da pasta views e etc...

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const app = express();
+const path = require('path')
 const checklistRounter = require('./src/rounter/checklist')
+const rootRounter = require('./src/rounter/index')
 require('./config/database')
 
-// a middleware
+const app = express();
 app.use(express.json())
+
+app.use(express.static(path.join(__dirname , 'public')))
+
+
+app.set('views' , path.join(__dirname, 'src/views'))
+app.set('view engine' , 'ejs')
+// a middleware
+app.use('/' , rootRounter)
 app.use('/checklists' , checklistRounter)
 
 app.listen(9000, () => {
