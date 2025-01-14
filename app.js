f2e0c2e6d0986caaec13b7facca5c6f9c2f03619
jesse <nitchsx@gmail.com>
2025-01-14 12:08:03 -0300

    a

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
-const express = require('express');
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const checklistRounter = require('./src/rounter/checklist')
-const taskRouter = require('./src/rounter/task')
+const checklistRounter = require("./src/rounter/checklist");
+const taskRouter = require("./src/rounter/task");
 
-const rootRounter = require('./src/rounter/index')
-const methodOverride = require('method-override')
+const rootRounter = require("./src/rounter/index");
+const methodOverride = require("method-override");
 
-require('./config/database')
-
+require("./config/database");
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method', { methods: ['POST', 'GET']}));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
 
-app.use(express.static(path.join(__dirname , 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.set('views' , path.join(__dirname, 'src/views'))
-app.set('view engine' , 'ejs')
+app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "ejs");
 // a middleware
-app.use('/' , rootRounter)
-app.use('/checklists' , checklistRounter)
-app.use('/checklists' , taskRouter.checklistDependent)
+app.use("/", rootRounter);
+app.use("/checklists", checklistRounter);
+app.use("/checklists", taskRouter.checklistDependent);
 
-app.listen(9000, () => {
-  console.log('Servidor iniciado')
-})
+app.listen(3000, () => {
+  console.log("Servidor iniciado");
+});
